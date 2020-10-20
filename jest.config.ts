915837276e5369518bd27994a7e792b5365511b1
kyoncy <kyota.n.2014@gmail.com>
2020-10-20 21:38:08 +0900

    update package & add jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  testMatch: [
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
+};
+
+export default config;
