802feb09d2a8dcf791312c695d70932a1309dab2
kyoncy <kyota.n.2014@gmail.com>
2020-10-27 07:35:04 +0900

    remove type-assertion & add type-predicate

--- src/util/pickHeadingFromAst.ts
+++ src/util/pickHeadingFromAst.ts
@@ -5,8 +5,9 @@ const pickHeadingFromAST = (
   headingDepth: 1 | 2 | 3 | 4 | 5 | 6 = 6
 ): Heading[] => {
   return markdownAst.filter(
-    (item) => item.type === 'heading' && item.depth <= headingDepth
-  ) as Heading[]
+    (item): item is Heading =>
+      item.type === 'heading' && item.depth <= headingDepth
+  )
 }
 
 export default pickHeadingFromAST
