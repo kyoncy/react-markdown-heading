3894a4d0066c40df107b8e2c2fbe8198bf129e1b
kyoncy <kyota.n.2014@gmail.com>
2020-10-27 22:52:40 +0900

    refactor util/parseHeadingAST.ts

--- src/util/parseHeadingAST.ts
+++ src/util/parseHeadingAST.ts
@@ -8,32 +8,25 @@ export interface HeadingWithId extends Heading {
 const parseHeadingAST = (headingAST: Heading[]): HeadingWithId[] => {
   const headingList: HeadingWithId[] = []
 
-  let currentDepth = 0
-  let currentParent = 0
-  let currentHeading: HeadingWithId
+  let depth: 0 | 1 | 2 | 3 | 4 | 5 | 6 = 0
+  let parentId = 0
+  let heading: HeadingWithId
 
   headingAST.forEach((item, index) => {
-    currentHeading = Object.assign(item, { id: index + 1, parentId: 0 })
-    if (item.depth > currentDepth) {
-      currentDepth = item.depth
-      currentHeading.parentId = index
-      currentParent++
-    } else {
-      currentDepth = item.depth
-      const sameDepthList = headingList.filter(
-        (item) => item.depth === currentDepth
-      )
-      if (sameDepthList.length > 0) {
-        currentHeading.parentId =
-          sameDepthList[sameDepthList.length - 1].parentId
-        currentParent = currentHeading.parentId
-      } else {
-        currentHeading.parentId = 0
-        currentParent = 0
-      }
-    }
+    heading = Object.assign(item, { id: index + 1, parentId: 0 })
 
-    headingList.push(currentHeading)
+    const sames = headingList.filter(
+      (itemWithId) => itemWithId.depth === item.depth
+    )
+    heading.parentId =
+      item.depth > depth
+        ? index
+        : sames.length && sames[sames.length - 1].parentId
+    parentId =
+      item.depth > depth ? parentId + 1 : sames.length && heading.parentId
+    depth = item.depth
+
+    headingList.push(heading)
   })
 
   return headingList
