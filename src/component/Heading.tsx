1f3e5098d7aa4deef7f4c5d568cc2bd3a0142851
kyoncy <kyota.n.2014@gmail.com>
2021-03-30 10:00:48 +0900

    :rocket: Add React.memo

--- src/component/Heading.tsx
+++ src/component/Heading.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React, { FC } from 'react'
 import { Heading } from '../util/parseHeadingText'
 
@@ -10,43 +11,45 @@ interface HeadingProps {
   hyperlink?: boolean
 }
 
-const Heading: FC<HeadingProps> = ({
-  headingList,
-  rootId = 0,
-  ulClassName,
-  liClassName,
-  anchorClassName,
-  hyperlink = false,
-}) => {
-  const filteredList = headingList.filter((item) => item.parentId === rootId)
+const Heading: FC<HeadingProps> = React.memo(
+  ({
+    headingList,
+    rootId = 0,
+    ulClassName,
+    liClassName,
+    anchorClassName,
+    hyperlink = false,
+  }) => {
+    const filteredList = headingList.filter((item) => item.parentId === rootId)
 
-  return filteredList.length ? (
-    <ul className={ulClassName}>
-      {filteredList.map((item, index) => {
-        const { text, href } = item
-        const element = hyperlink ? (
-          <a href={href} className={anchorClassName}>
-            {text}
-          </a>
-        ) : (
-          text
-        )
+    return filteredList.length ? (
+      <ul className={ulClassName}>
+        {filteredList.map((item, index) => {
+          const { text, href } = item
+          const element = hyperlink ? (
+            <a href={href} className={anchorClassName}>
+              {text}
+            </a>
+          ) : (
+            text
+          )
 
-        return (
-          <li key={index} className={liClassName}>
-            {element}
-            <Heading
-              headingList={headingList}
-              rootId={item.id}
-              ulClassName={ulClassName}
-              liClassName={liClassName}
-              hyperlink={hyperlink}
-            />
-          </li>
-        )
-      })}
-    </ul>
-  ) : null
-}
+          return (
+            <li key={index} className={liClassName}>
+              {element}
+              <Heading
+                headingList={headingList}
+                rootId={item.id}
+                ulClassName={ulClassName}
+                liClassName={liClassName}
+                hyperlink={hyperlink}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    ) : null
+  }
+)
 
 export default Heading
