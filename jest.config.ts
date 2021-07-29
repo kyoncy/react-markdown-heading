ff1679a1ad8dde42a0e40f514f070e5e5be69379
kyoncy <k.nagao@hitto.co.jp>
2021-07-30 08:36:10 +0900

    :package: Upgrade devDependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
+  testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
